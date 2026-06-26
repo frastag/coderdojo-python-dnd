fdc635f3f5a616fe717a6afa0ea080b5ef36c481
francesca <frafra82@gmail.com>
2026-06-26 14:52:22 +0200

    togliere main

--- 01_eroe/01_eroe.py
+++ 01_eroe/01_eroe.py
@@ -1,15 +1,11 @@
-def main():
-    nome = input("Inserisci il nome del tuo personaggio\n")
-    classe = input("Inserisci la classe del tuo personaggio: Mago, Guerriero, Ladro\n")
-    if classe != "Guerriero" and classe != "Ladro" and classe != "Mago":
-        classe = "Mago"  # se la classe inserita non è tra quelle disponibili, viene assegnata una classe di default
-    vita = 20
-    forza = 5
-    magia = 15
-    animale = "Topo"
-    print("Benvenuto", nome, "la classe del tuo personaggio è", classe, "e hai", vita, "punti vita,", forza,
-          "punti forza,", magia, "punti magia e come animale hai", animale)
+nome = input("Inserisci il nome del tuo personaggio\n")
+classe = input("Inserisci la classe del tuo personaggio: Mago, Guerriero, Ladro\n")
+if classe != "Guerriero" and classe != "Ladro" and classe != "Mago":
+    classe = "Mago"  # se la classe inserita non è tra quelle disponibili, viene assegnata una classe di default
+vita = 20
+forza = 5
+magia = 15
+animale = "Topo"
+print("Benvenuto", nome, "la classe del tuo personaggio è", classe, "e hai", vita, "punti vita,", forza,
+      "punti forza,", magia, "punti magia e come animale hai", animale)
 
-
-if __name__ == "__main__":
-    main()
--- 02_combattimento/02_combattimento.py
+++ 02_combattimento/02_combattimento.py
@@ -1,43 +1,37 @@
 import random
 
-def main():
-    classi_disponibili = ["guerriero", "ladro", "mago"]
-    vita = 20
+classi_disponibili = ["guerriero", "ladro", "mago"]
+vita = 20
 
-    nome = input("Inserisci il nome del tuo personaggio\n")
-    classe = input("Inserisci la classe del tuo personaggio: Mago, Guerriero, Ladro\n")
+nome = input("Inserisci il nome del tuo personaggio\n")
+classe = input("Inserisci la classe del tuo personaggio: Mago, Guerriero, Ladro\n")
 
-    # miglioramento rispetto alla versione scorsa: controlliamo se la classe inserita dall'utente è
-    # corretta, usando una lista di classi disponibili e controllando se quanto inserito dall'utente
-    # non è al suo interno e rendendo il tutto case insensitive
-    if not classe.lower() in classi_disponibili: # uso lower per non tenere in considerazione se le lettere sono maiuscole o minuscole
-        # se la classe non è tra quelle valide, viene selezionata in maniera random
-        classe = random.choice(classi_disponibili)
+# miglioramento rispetto alla versione scorsa: controlliamo se la classe inserita dall'utente è
+# corretta, usando una lista di classi disponibili e controllando se quanto inserito dall'utente
+# non è al suo interno e rendendo il tutto case insensitive
+if not classe.lower() in classi_disponibili: # uso lower per non tenere in considerazione se le lettere sono maiuscole o minuscole
+    # se la classe non è tra quelle valide, viene selezionata in maniera random
+    classe = random.choice(classi_disponibili)
 
-    # in base alla classe cambiano i punti forza e magia. Vita sono sempre uguaali
-    if classe.lower() == "guerriero":
-        forza = 10
-        magia = 0
-    elif classe.lower() == "ladro":
-        forza = 5
-        magia = 5
-    else:
-        forza = 0
-        magia = 10
+# in base alla classe cambiano i punti forza e magia. Vita sono sempre uguaali
+if classe.lower() == "guerriero":
+    forza = 10
+    magia = 0
+elif classe.lower() == "ladro":
+    forza = 5
+    magia = 5
+else:
+    forza = 0
+    magia = 10
 
-    print("Benvenuto", nome, "la classe del tuo personaggio è", classe, "e hai", vita, "punti vita,", forza,
-          "punti forza,", magia, "punti magia.")
+print("Benvenuto", nome, "la classe del tuo personaggio è", classe, "e hai", vita, "punti vita,", forza,
+      "punti forza,", magia, "punti magia.")
 
-    # Esempio di attacco e difesa nemico
-    attacco = 10
-    difesa_nemico = 3
+# Esempio di attacco e difesa nemico
+attacco = 10
+difesa_nemico = 3
 
-    if attacco > difesa_nemico:
-        print("Attacco perfetto!")
-    elif attacco <= difesa_nemico:
-        print("Il memico ha parato il tuo attacco!")
-
-
-
-if __name__ == "__main__":
-    main()
+if attacco > difesa_nemico:
+    print("Attacco perfetto!")
+elif attacco <= difesa_nemico:
+    print("Il memico ha parato il tuo attacco!")
